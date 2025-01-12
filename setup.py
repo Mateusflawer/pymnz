d6bb372c1cd52eab246315aa36b0c61522e69444
MateusFlawer <mateusflawer@gmail.com>
2025-01-12 19:26:37 -0300

    `Removed test files and updated package metadata`

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pymnz",
-    version="0.1.2",
+    version="0.1.3",
     author="Mateus Menezes",
     author_email="mateusflawer@gmail.com",
     description=(
