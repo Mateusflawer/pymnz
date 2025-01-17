7cee2691940604166c3f2833828b3946e9201d69
Mateus Alves <111348801+Mateusflawer@users.noreply.github.com>
2025-01-17 18:25:24 -0300

    `Update pymnz package to version 0.1.8`

--- teste.py
+++ teste.py
@@ -1,4 +1,11 @@
-from pymnz import database
+import pymnz
 
 
-database.unique_column(1, 'teste', 'test')
+@pymnz.utils.errors.retry_on_failure(10)
+def main():
+    print('Vou dar erro em seguida')
+    raise Exception('Erro de exemplo')
+
+
+if __name__ == "__main__":
+    pymnz.models.Script('Bot test', main).run()
