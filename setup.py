37db6a225c37f040259277a641779406ee8f8a20
Mateus Alves <111348801+Mateusflawer@users.noreply.github.com>
2025-01-21 15:03:17 -0300

    "Updated version number in setup.py from 0.1.8 to 0.1.9."

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pymnz",
-    version="0.1.8",
+    version="0.1.9",
     author="Mateus Menezes",
     author_email="mateusflawer@gmail.com",
     description=(
