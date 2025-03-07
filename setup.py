f0a7f87036db72450f0f23300622800f22ada555
Mateus Alves <111348801+Mateusflawer@users.noreply.github.com>
2025-03-07 10:09:37 -0300

    atualização da versão

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pymnz",
-    version="0.2.9",
+    version="0.3.0",
     author="Mateus Menezes",
     author_email="mateusflawer@gmail.com",
     description=(
