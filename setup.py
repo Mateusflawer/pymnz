4d68ee0d7447076c4bcc2f006db4d0cb90445e66
Mateus Menezes <111348801+Mateusflawer@users.noreply.github.com>
2025-03-27 17:02:13 -0300

    ajuste na versão!

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pymnz",
-    version="0.3.4",
+    version="0.3.5",
     author="Mateus Menezes",
     author_email="mateusflawer@gmail.com",
     description=(
