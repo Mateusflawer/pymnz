c859a0d8976543ea5bdcfc3c71b44757431e18d6
Mateus Menezes <111348801+Mateusflawer@users.noreply.github.com>
2025-03-27 18:15:50 -0300

    Atualização de versão para subir para nuvem!

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pymnz",
-    version="0.3.5",
+    version="0.3.6",
     author="Mateus Menezes",
     author_email="mateusflawer@gmail.com",
     description=(
